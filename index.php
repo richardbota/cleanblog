180b366683d2950d84b3927c472283764769c58a
Richard Bota <richard.bota@khk.ee>
2015-10-09 10:46:33 +0300

    Blogipostituse esilehe dünaamiliseks

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+
+// Connect database
+$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+
+
+// Retrieve data from database
+$posts = [];
+$q = mysqli_query($db, "SELECT * FROM posts NATURAL JOIN authors");
+while ($row = mysqli_fetch_assoc($q)) {
+    $posts[] = $row;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +24,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Clean Blog - About</title>
+    <title>Clean Blog</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -22,8 +37,9 @@
           type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet'
           type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-          rel='stylesheet' type='text/css'>
+    <link
+        href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -75,14 +91,14 @@
 
 <!-- Page Header -->
 <!-- Set your background image for this header on the line below. -->
-<header class="intro-header" style="background-image: url('img/about-bg.jpg')">
+<header class="intro-header" style="background-image: url('img/home-bg.jpg')">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="page-heading">
-                    <h1>About Me</h1>
+                <div class="site-heading">
+                    <h1>Clean Blog</h1>
                     <hr class="small">
-                    <span class="subheading">This is what I do.</span>
+                    <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
                 </div>
             </div>
         </div>
@@ -93,17 +109,33 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates
-                odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus
-                consectetur?</p>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque
-                architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in
-                officia voluptas voluptatibus, minus!</p>
+            <? foreach ($posts as $post) : ?>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestiae debitis nobis, quod sapiente
-                qui voluptatum, placeat magni repudiandae accusantium fugit quas labore non rerum possimus, corrupti
-                enim modi! Et.</p>
+                <div class="post-preview">
+
+                    <a href="post.php">
+                        <h2 class="post-title"><?= $post['post_title'] ?></h2>
+
+                        <h3 class="post-subtitle"><?= $post['post_description'] ?></h3>
+                    </a>
+
+                    <p class="post-meta">Posted by <a href="#"><?= $post['author_name'] ?></a>
+                        on <?= $post['post_created'] ?></p>
+
+                </div>
+
+                <hr>
+
+            <?php endforeach ?>
+
+
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="next">
+                    <a href="#">Older Posts &rarr;</a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
