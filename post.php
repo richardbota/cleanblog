defb2dafa7b6448348d6f38836712d4b6df1928d
Richard Bota <richard.bota@khk.ee>
2015-10-10 10:09:22 +0300

    Ühe postituse vaade [Delivers #105076682]

--- post.php
+++ post.php
@@ -1,6 +1,8 @@
 <?php
 
-$post_id = 2;
+//Get post id
+$post_id = isset($_GET['id']) ? $_GET['id'] : -1;
+
 // Connect database
 $db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
