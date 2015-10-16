8ec172d4219bfb98a04930f686116fa46bdb36ab
Richard Bota <richard.bota@khk.ee>
2015-10-16 14:33:56 +0300

    debug

--- include/database.php
+++ include/database.php
@@ -12,6 +12,4 @@ require_once 'config.php';
 $db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
 
-require 'config.php';
-
 ?>
